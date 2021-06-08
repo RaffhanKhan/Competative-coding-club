93a5718a61816a6a6e825eb920a96b38646ec647
RaffhanKhan <48751738+RaffhanKhan@users.noreply.github.com>
2021-06-08 10:38:25 +0530

    Update settings.py

--- CodingClub/settings.py
+++ CodingClub/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '=prt*5ae&(*#y5r)m59*+@harx!#=*0se-p6=w-t7-ue==%7#w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
